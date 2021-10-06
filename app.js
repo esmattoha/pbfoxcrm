41031a9d1e20e168cf43053fff5531c4b3e5f70c
esmattoha <mondaldipu904@gmail.com>
2021-10-06 12:05:13 +0530

    Database : Mongodb connect

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 // Import Dependencies
+import env from "dotenv";
 import express from "express";
-
+import mongoose from "mongoose";
 
 const app = express();
+env.config();  // env configuration
 
 
-// Export 
-export default app ;
+// DataBase Connection
+try {
+  (async () => {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("You are Successfully connected with Database.");
+  })();
+} catch (error) {
+  console.log(error);
+  process.exit(1);
+}
+
+// Export
+export default app;
