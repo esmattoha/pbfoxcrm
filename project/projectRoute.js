986384dbad29f6c83cdf45545bba34bda1b8bc4b
esmattoha <mondaldipu904@gmail.com>
2021-10-09 14:15:49 +0530

    project curd operation

--- project/projectRoute.js
+++ project/projectRoute.js
@@ -28,5 +28,68 @@ projectRoute.post(
   })
 );
 
+projectRoute.get(
+  "/",
+  catchAsync(async (req, res, next) => {
+    const projects = await Project.find({});
+
+    if (projects.length <= 0) {
+      return next(new AppError(`Resource not found.`, 404));
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: projects,
+    });
+  })
+);
+
+projectRoute.get(
+  "/:id",
+  catchAsync(async (req, res, next) => {
+    const project = await Project.findById(req.params.id);
+
+    if (!project) {
+      return next(new AppError(`Resource not found.`, 404));
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: project,
+    });
+  })
+);
+
+projectRoute.patch(
+  "/:id",
+  catchAsync(async (req, res, next) => {
+    const project = await Project.findByIdAndUpdate(req.params.id, req.body);
+
+    if (!project) {
+      return next(new AppError(`Resource not found.`, 404));
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: project,
+    });
+  })
+);
+
+projectRoute.delete(
+  "/:id",
+  catchAsync(async (req, res, next) => {
+    const project = await Project.findByIdAndDelete(req.params.id);
+
+    if (!project) {
+      return next(new AppError(`Resource not found.`, 404));
+    }
+
+    res.status(200).json({
+      status: "success",
+    });
+  })
+);
+
 // export
-export default projectRoute ;
+export default projectRoute;
